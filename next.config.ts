e2b8510d092616aecd81a492706d2c297392878a
Macbook <macbook@MacBook-Air-Macbook.local>
2026-06-11 14:06:56 +0300

    fix: set basePath for github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/bavaria" : "",
   images: {
     unoptimized: true,
   },
